bf3c86290f577a4afe9bdd0d5a70884ea87577fb
Ivan Soregashi <8psycho@gmail.com>
2024-07-28 21:23:04 +0500

    moved everything back together, disabled API as of now Form works

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from datetime import datetime
 
-from models.transaction import TransactionModel
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///transactions.sqlite3"
@@ -14,7 +12,55 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 db = SQLAlchemy(app)
 api = Api(app)
 
-api.add_resource(TransactionModel, "/transaction")
+
+class TransactionModel(db.Model):
+    __tablename__ = 'transactions'
+
+    _id = db.Column("id", db.Integer, primary_key=True)
+
+    date_time = db.Column(db.DateTime)
+    type = db.Column(db.String(20))
+    account = db.Column(db.String(20))
+    currency = db.Column(db.String(3))
+    amount = db.Column(db.Integer)
+    category = db.Column(db.String(50))
+    comment = db.Column(db.String(100))
+
+    def __init__(self, date_time, type, account, currency, amount, category, comment):
+        self.date_time = date_time
+        self.type = type
+        self.account = account
+        self.currency = currency
+        self.amount = amount
+        self.category = category
+        self.comment = comment
+        # add validation
+
+    def json(self):
+        return {
+             'date_time': self.date_time,
+             'type': self.type,
+             'account': self.account,
+             'currency': self.currency,
+             'amount': self.amount,
+             'category': self.category,
+             'comment': self.comment,
+        }
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''@classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()'''
+
+
+#api.add_resource(TransactionModel, "/transaction")
 
 
 @app.route("/")
