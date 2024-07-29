a04e0585e7febbad42b10dd9b3da3a9ad204207a
Ivan Soregashi <8psycho@gmail.com>
2024-07-29 21:47:42 +0500

    working on tests again, still get an error

--- tests/base_test.py
+++ tests/base_test.py
@@ -12,14 +12,18 @@ from db import db
 
 
 class BaseTest(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"
         with app.app_context():
             db.init_app(app)
+
+    def setUp(self):
+        with app.app_context():
             db.create_all()
 
-        self.app = app.test_client()
+        self.app = app.test_client
         self.app_context = app.app_context
 
     def tearDown(self):
--- tests/system/test_home.py
+++ tests/system/test_home.py
@@ -3,7 +3,7 @@ from tests.base_test import BaseTest
 
 class TestHome(BaseTest):
     def test_home(self):
-        with self.app() as c:
-            response = c.get("/")
-
-            self.assertEqual(response.status_code, 200)
+        with self.app() as client:
+            with self.app_context():
+                response = client.get("/")
+                self.assertEqual(response.status_code, 200)
